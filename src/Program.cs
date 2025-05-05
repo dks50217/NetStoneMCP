816937364b64dd859012fd24a80488bb5f193af3
Michael.Chang <Michael.Chang@logikco.com>
2025-05-05 22:07:06 +0800

    init

---
+++ src/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NetStoneMCP.Services;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services
+    .AddMcpServer()
+    .WithStdioServerTransport()
+    .WithToolsFromAssembly();
+
+builder.Services.AddMemoryCache();
+
+builder.Services.AddLogging(builder =>
+{
+    builder.AddConsole();
+    builder.SetMinimumLevel(LogLevel.Information);
+});
+
+builder.Services.AddSingleton<INetStoneService, NetStoneService>();
+
+static async Task InitNetStone(IHost host)
+{
+    var netStoneService = host.Services.GetRequiredService<INetStoneService>();
+    await netStoneService.InitializeAsync();
+}
+
+var app = builder.Build();
+
+await InitNetStone(app);
+
+await app.RunAsync();
+
---
+++ src/Services/NetStoneService.cs
@@ -0,0 +1,78 @@
+﻿using NetStone.Model.Parseables.FreeCompany.Members;
+using NetStone.Model.Parseables.Search.Character;
+using NetStone.Model.Parseables.Search.FreeCompany;
+using NetStone.Search.Character;
+using NetStone.Search.FreeCompany;
+using NetStone;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetStoneMCP.Services
+{
+    public interface INetStoneService
+    {
+        Task InitializeAsync();
+        Task<FreeCompanySearchEntry?> GetFCInformation(string name, string world);
+        Task<CharacterSearchEntry?> GetCharacterID(string name, string world);
+        Task<IEnumerable<FreeCompanyMembersEntry>?> GetFCMembers(string id);
+    }
+
+    public class NetStoneService : INetStoneService
+    {
+        private LodestoneClient? _lodestoneClient;
+
+        public async Task InitializeAsync()
+        {
+            _lodestoneClient = await LodestoneClient.GetClientAsync();
+        }
+
+        public async Task<CharacterSearchEntry?> GetCharacterID(string name, string world)
+        {
+            if (_lodestoneClient == null)
+                throw new Exception("LodestoneClient initialization failed.");
+
+            var searchResponse = await _lodestoneClient.SearchCharacter(new CharacterSearchQuery()
+            {
+                CharacterName = name,
+                World = world
+            });
+
+            var lodestoneCharacter =
+                searchResponse?.Results
+                .FirstOrDefault(entry => entry.Name == name);
+
+            return lodestoneCharacter;
+        }
+
+        public async Task<FreeCompanySearchEntry?> GetFCInformation(string name, string world)
+        {
+            if (_lodestoneClient == null)
+                throw new Exception("LodestoneClient initialization failed.");
+
+            var searchResponse = await _lodestoneClient.SearchFreeCompany(new FreeCompanySearchQuery()
+            {
+                Name = name,
+                World = world
+            });
+
+            var freeCompany = searchResponse?.Results.FirstOrDefault(entry => entry.Name == name);
+
+            return freeCompany;
+        }
+
+        public async Task<IEnumerable<FreeCompanyMembersEntry>?> GetFCMembers(string id)
+        {
+            if (_lodestoneClient == null)
+                throw new Exception("LodestoneClient initialization failed.");
+
+            var searchResponse = await _lodestoneClient.GetFreeCompanyMembers(id: id);
+
+            var freeCompanyMembers = searchResponse?.Members;
+
+            return freeCompanyMembers;
+        }
+    }
+}
